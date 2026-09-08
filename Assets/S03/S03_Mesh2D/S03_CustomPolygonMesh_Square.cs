22aaba1853c9f695d187fab730a2a2bf9279df30
jjihye-0108 <wjswlgp9511@gmail.com>
2026-09-08 13:23:23 +0900

    Initial Unity project setup

---
+++ Assets/S03/S03_Mesh2D/S03_CustomPolygonMesh_Square.cs
@@ -0,0 +1,33 @@
+﻿  using UnityEngine;
+
+  [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+  public class S03_CustomPolygonMesh_Square : MonoBehaviour
+  {
+      void Start()
+      {
+          // TODO 1: 원하는 다각형의 정점 좌표를 채우세요 (최소 4개)
+          Vector3[] vertices = new Vector3[]
+          {
+              new Vector3(0f, 1f, 0f), //0
+              new Vector3(1f, 1f, 0f), //1
+              new Vector3(1f, 0f, 0f), //2
+              new Vector3(0f, 0f, 0f), //3
+              new Vector3(0f, 0f, 1f), //4
+          };
+
+          // TODO 2: 정점 3개씩 묶어 삼각형들을 구성하세요
+          int[] triangles = new int[]
+          {
+              0, 1, 2,
+              0, 2, 3,
+          };
+
+          Mesh mesh = new Mesh();
+          mesh.vertices = vertices;
+          mesh.triangles = triangles;
+          mesh.RecalculateNormals();
+
+          GetComponent<MeshFilter>().mesh = mesh;
+          GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+      }
+  }
---
+++ Assets/S03/S03_Mesh2D/S03_SingleTriangleMesh_Finish.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_SingleTriangleMesh_Finish : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f), // 0
+            new Vector3(1f, 0f, 0f), // 1
+            new Vector3(1f, 1f, 0f), // 2
+            new Vector3(0f, 1f, 0f), // 3
+            new Vector3(0f, 0f, 1f), // 4
+            new Vector3(1f, 0f, 1f), // 5
+            new Vector3(1f, 1f, 1f), // 6
+            new Vector3(0f, 1f, 1f), // 7
+        };
+
+        int[] triangles = new int[3] { 4, 6, 5 };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
